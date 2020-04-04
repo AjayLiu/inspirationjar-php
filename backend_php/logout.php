d16fbcd21786e44d5d529fe9567e081a874d03df
AjayLiu <ajayliu2928@gmail.com>
2020-04-04 16:57:00 -0700

    Deletes cookie

--- backend_php/logout.php
+++ backend_php/logout.php
@@ -5,11 +5,13 @@
     //UNSET THE PAYLOAD
     $_SESSION['payload'] = null;
 
-    //DELETE SESSION COOKIE
+    //DELETE SESSION COOKIE FROM DATABASE
     $myCookie = $_COOKIE['session'];
-
     $sql = "DELETE FROM sessions WHERE session_id = '$myCookie'";
     $result = $mysqli->query($sql) or die("ouch, error");
-    
+
+    //DELETE THE COOKIE
+    setcookie('session', '' , time() - 3600);
+
     echo "SUCCESS";
 ?>
