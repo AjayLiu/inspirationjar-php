c0ee7870f92abebe8944b79be88f14be3a1da2fa
AjayLiu <ajayliu2928@gmail.com>
2020-04-23 12:31:45 -0700

    Update setup_connection.php

--- backend_php/setup_connection.php
+++ backend_php/setup_connection.php
@@ -1,8 +1,12 @@
 <?php
-	$servername = "23.251.154.38";
+	// $servername = "23.251.154.38";
+	// $username = "root";
+	// $password = "Aq2aJmx38mCw5PwI";
+	// $database_in_use = "encourageme";
+	$servername = "35.199.147.183";
 	$username = "root";
-	$password = "Aq2aJmx38mCw5PwI";
-	$database_in_use = "encourageme";
+	$password = "dfgMitqsdzx5zzfI";
+	$database_in_use = "inspirationjar";
 
 	// Create connection
 	$mysqli = new mysqli($servername, $username, $password, $database_in_use);
