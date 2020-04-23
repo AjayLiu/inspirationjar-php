4972f4c6155f073aa76fbf0c4d5d47f2e8358f3e
AjayLiu <ajayliu2928@gmail.com>
2020-04-22 21:44:26 -0700

    Update redirectLinks.php

--- backend_php/redirectLinks.php
+++ backend_php/redirectLinks.php
@@ -2,11 +2,11 @@
     $isLocal = false;
 
     $siteName = $isLocal ? "localhost" : "inspirationjar.com";
-    $http = $isLocal ? "http" : "http";
-    $submitLink = $http."://".$siteName."/submit.php";
-    $loginPageLink =  $http."://".$siteName."/login_page.php";
-    $afterLoginLink =  $http."://".$siteName."/backend_php/afterLogin.php";
-    $indexLink =  $http."://".$siteName."/index.php";
-    $accountLink =  $http."://".$siteName."/myaccount.php";
+    $http = $isLocal ? "http://" : "http://www.";
+    $submitLink = $http.$siteName."/submit.php";
+    $loginPageLink =  $http.$siteName."/login_page.php";
+    $afterLoginLink =  $http.$siteName."/backend_php/afterLogin.php";
+    $indexLink =  $http.$siteName."/index.php";
+    $accountLink =  $http.$siteName."/myaccount.php";
 
 ?>
