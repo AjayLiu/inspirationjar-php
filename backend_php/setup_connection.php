05dc0e48ea5c7627211a2d9f7c4f3de4244a610c
AjayLiu <ajayliu2928@gmail.com>
2020-05-08 15:50:08 -0700

    new database

--- backend_php/setup_connection.php
+++ backend_php/setup_connection.php
@@ -1,16 +1,16 @@
 <?php
 
 	//OLD SQL
-	$servername = "23.251.154.38";
-	$username = "root";
-	$password = "Aq2aJmx38mCw5PwI";
-	$database_in_use = "encourageme";
+	// $servername = "23.251.154.38";
+	// $username = "root";
+	// $password = "Aq2aJmx38mCw5PwI";
+	// $database_in_use = "encourageme";
 
 	//NEW SQL
-	// $servername = "35.199.147.183";
-	// $username = "root";
-	// $password = "dfgMitqsdzx5zzfI";
-	// $database_in_use = "inspirationjar";
+	$servername = "34.105.109.227";
+	$username = "root";
+	$password = "OI8z1HuxFEuj50N3";
+	$database_in_use = "inspirationjar";
 
 	// Create connection
 	$mysqli = new mysqli($servername, $username, $password, $database_in_use);
